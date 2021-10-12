0b604545a9c5cbb43a2a4892bacb780577f4b945
Nataliesmyth <nataliesmyth11@gmail.com>
2021-10-12 15:04:51 -0400

    last month days were off by one fixed

--- src/index.js
+++ src/index.js
@@ -2,11 +2,16 @@
 // import "./style.css";
 
 const date = new Date();
-date.setMonth(6);
+// date.setMonth(6);
+date.setDate(1);
+console.log(date.getDay());
 
 const month = date.getMonth();
 const monthDays = document.querySelector('.days')
 const lastDay = new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate();
+const prevLastDay = new Date(date.getFullYear(), date.getMonth(), 0).getDate();
+console.log(prevLastDay)
+const firstDayIndex = date.getDay();
 
 
 console.log(lastDay);
@@ -32,6 +37,9 @@ document.querySelector('.date p').innerHTML = date.toDateString();
 
 let days = '';
 
+for(let x = firstDayIndex; x > 0; x--){
+  days += `<div class="prev-date">${prevLastDay - x + 1}</div>`
+}
 for(let i = 1; i <= lastDay; i++) {
   days += `<div>${i}</div>`
 };
