39ccc9129498ca720c23d05dac8ad15f1663b760
Nataliesmyth <nataliesmyth11@gmail.com>
2021-10-12 15:19:20 -0400

    current date highlighted

--- src/index.js
+++ src/index.js
@@ -13,9 +13,15 @@ const prevLastDay = new Date(date.getFullYear(), date.getMonth(), 0).getDate();
 console.log(prevLastDay)
 const firstDayIndex = date.getDay();
 
+const lastDayIndex = new Date(
+  date.getFullYear(),
+  date.getMonth() + 1,
+  0
+).getDay();
 
-console.log(lastDay);
+const nextDays = 7 - lastDayIndex - 1;
 
+console.log(nextDays)
 
 const months = [
   'January',
@@ -40,7 +46,16 @@ let days = '';
 for(let x = firstDayIndex; x > 0; x--){
   days += `<div class="prev-date">${prevLastDay - x + 1}</div>`
 }
+
 for(let i = 1; i <= lastDay; i++) {
-  days += `<div>${i}</div>`
+  if(i === new Date().getDate() && date.getMonth() === new Date().getMonth()) {
+    days += `<div class="today">${i}</div>`;
+  } else {
+    days += `<div>${i}</div>`
+  }
 };
+
+for (let j = 1; j <= nextDays; j++) {
+  days += `<div class="next-date">${j}</div>`;
+}
 monthDays.innerHTML = days;
